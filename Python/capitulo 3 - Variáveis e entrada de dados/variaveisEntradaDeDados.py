5db17cc1300edfdc67b51d53afc7a2a459714d5f
carlosmartins1985 <carlostenzel@gmail.com>
2026-05-04 21:48:18 -0300

    add: Capitulo 03, variaveis e entrada de dados

---
+++ Python/capitulo 3 - Variáveis e entrada de dados/variaveisEntradaDeDados.py
@@ -0,0 +1,12 @@
+"""
+        Variáveis núméricas
+    Nomes de variáveis não podem começar com números, não podem ter espaços, podem 
+    conter números e sublinhas. Podemos inclusive usar variáveis com acento.
+    Em Python podemos representar números na base binária com valores 2^1000000, mesmo quando 
+    o resultado é um número de 301030 dígitos.
+"""
+
+# Neste cálculo temos um problema de precisão, que se necessário devemos consultar a 
+# documentação para representa-lo melhor.
+print(3 * 0.1)
+# 0.30000000000000004
